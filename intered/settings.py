e33effdb1303ab72857925e871ff0be4e12fba1a
Allen Arcenal <edward.arcenal@orangeandbronze.com>
2018-08-10 18:24:34 +0800

    fix media settings. fixed layout

--- intered/settings.py
+++ intered/settings.py
@@ -121,11 +121,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'registration/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'registration/static')
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'media/')
 )
 
 EMAIL_USE_TLS = True
