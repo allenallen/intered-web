6477046d049445f4dbd3a6273a6c7f3a624ef831
Allen Arcenal <edward.arcenal@orangeandbronze.com>
2018-08-10 17:29:43 +0800

    initial

---
+++ registration/models.py
@@ -0,0 +1,111 @@
+import io
+import os
+
+import qrcode
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.urls import reverse
+from django.utils import timezone
+
+image_storage = FileSystemStorage(
+    location=u'{0}/logos/'.format(settings.MEDIA_ROOT),
+    base_url=u'{0}logos/'.format(settings.MEDIA_URL),
+)
+
+
+def image_directory_path(instance, filename):
+    return u'picture/{0}'.format(filename)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Event Name')
+    logo = models.ImageField(verbose_name='Logo for the event', upload_to=image_directory_path, storage=image_storage)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    event_registration_url = models.URLField(verbose_name="Event Registration Link", null=True, blank=True)
+    event_added = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} ({self.start_date} - {self.end_date})'
+
+
+@receiver(post_save, sender=Event)
+def createUrlLink(sender, instance, created, **kwargs):
+    if instance.event_added is False:
+        instance.generate_qrcode()
+        instance.event_registration_url = reverse('register', args=[str(instance.pk)])
+        instance.event_added = True
+        instance.save()
+
+
+class ShsTrack(models.Model):
+    code = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.description
+
+
+class SchoolList(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    last_name = models.CharField(max_length=100, verbose_name="Last Name")
+    first_name = models.CharField(max_length=200, verbose_name="First Name")
+    school = models.ForeignKey(SchoolList, on_delete=models.SET_NULL, blank=True, null=True)
+    shs_track = models.ForeignKey(ShsTrack, on_delete=models.SET_NULL, blank=True, null=True,
+                                  verbose_name="Current SHS Track")
+    projected_course = models.CharField(max_length=200, help_text="First choice of Course", verbose_name="Course")
+    email = models.EmailField()
+    date_of_birth = models.DateField(verbose_name="Date of Birth")
+    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
+    qr_code = models.ImageField(null=True, upload_to='qrcode')
+    date_registered = models.DateTimeField(editable=False)
+    date_modified = models.DateTimeField()
+    qr_added = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date_registered = timezone.now()
+            self.date_modified = timezone.now()
+            if self.qr_added is False:
+                self.qr_added = True
+                self.generate_qrcode()
+        return super(Student, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+    def get_absolute_url(self):
+        return reverse('student-detail', args=[str(self.id)])
+
+    def generate_qrcode(self):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=6,
+            border=4,
+        )
+        qr.add_data(f'{self.email}|{self.first_name}|{self.last_name}')
+        qr.make(fit=True)
+
+        img = qr.make_image()
+
+        buffer = io.BytesIO()
+        img.save(buffer)
+        buffer.seek(0, os.SEEK_END)
+        filename = f'student-{self.last_name}.png'
+        filebuffer = InMemoryUploadedFile(
+            buffer, None, filename, 'image/png', buffer.tell(), None)
+        self.qr_code.save(filename, filebuffer)
+
+    class Meta:
+        ordering = ['id']
---
+++ registration/views.py
@@ -0,0 +1,64 @@
+from django.core.mail import send_mail, EmailMultiAlternatives, EmailMessage
+from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+
+from registration.forms import RegistrationForm
+from registration.models import Event, Student
+
+
+def registration(request, pk):
+    print('HERE')
+    print(request.method)
+    event = Event.objects.get(pk=pk)
+    if request.method == 'POST':
+        print('HERE POST')
+        form = RegistrationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            print('HERE VALID')
+            last_name = form.cleaned_data['last_name']
+            first_name = form.cleaned_data['first_name']
+            school = form.cleaned_data['school']
+            shs_track = form.cleaned_data['shs_track']
+            projected_course = form.cleaned_data['projected_course']
+            email = form.cleaned_data['email']
+            date_of_birth = form.cleaned_data['date_of_birth']
+            gender = form.cleaned_data['gender']
+
+            student = Student()
+            student.last_name = last_name
+            student.first_name = first_name
+            student.school = school
+            student.shs_track = shs_track
+            student.projected_course = projected_course
+            student.email = email
+            student.date_of_birth = date_of_birth
+            student.gender = gender
+            student.save()
+
+            html_message = render_to_string('email_template.html', {'context': {'qr_string': student.email}})
+            print(html_message)
+            msg = EmailMessage('Thank You', html_message, 'allenarcenal@gmail.com', [student.email])
+
+            msg.attach(student.qr_code.name, student.qr_code.read(), 'image/png')
+
+            msg.content_subtype = 'html'
+            msg.send()
+
+            return render(request, 'success.html', context={'student': student,
+                                                            'event_name': event.name,
+                                                            'event_id': event.id,
+                                                            'event_logo': event.logo})
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'event_name': event.name,
+        'event_id': event.id,
+        'event_logo': event.logo,
+        'event_start_date': event.start_date,
+        'event_end_date': event.end_date,
+        'form': form
+    }
+    print(context)
+    return render(request, 'registration.html', context=context)
